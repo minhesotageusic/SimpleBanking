8d0271ffc79577029754d15fd7a5a4470b3739cf
Minhesota Geusic <71162128+minhesotageusic@users.noreply.github.com>
2021-09-12 08:53:24 -0400

    Create AccountNode.java

---
+++ SimpleBanking/Main/AccountNode.java
@@ -0,0 +1,10 @@
+package Main;
+
+public class AccountNode {
+	public Account account;
+	public AccountType accountType;
+	public AccountNode (Account account, AccountType accountType) {
+		this.account = account;
+		this.accountType = accountType;
+	}
+}
